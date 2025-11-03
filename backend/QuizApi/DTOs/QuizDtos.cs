8d0730dbcbf00bacefd1cb476aa0021e2e62b7e5
heaur <heaur5362@oslomet.no>
2025-11-03 18:18:11 +0100

    Lagt til namespace i DTO filer

--- backend/QuizApi/DTOs/QuizDtos.cs
+++ backend/QuizApi/DTOs/QuizDtos.cs
@@ -1,2 +1,6 @@
+namespace QuizApi.DTOs
+{
 public record QuizSetCreateDto(string Title, string? Description);
 public record QuizSetReadDto(int Id, string Title, string? Description, DateTime CreatedAt);
+}
+
